def361d505d856554f9a2f49bcf48da85af938db
jenniii-om <141550650+jenniii-om@users.noreply.github.com>
2024-07-01 02:07:17 +1000

    python test

--- test.py
+++ test.py
@@ -393,3 +393,5 @@ def snake_case(text):
     raise TypeError("The snake_case() function expects a string as an argument, please check the data type provided.")
     
 snake_case("User Name 187")
+
+###
