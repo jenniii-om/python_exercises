33b0b0ad129023d0790b5afd43757bbfd408d7b7
jenniii-om <141550650+jenniii-om@users.noreply.github.com>
2024-08-07 11:01:07 +1000

    added import exercise for stata file

--- importing_data.py
+++ importing_data.py
@@ -278,3 +278,71 @@ df2 = xls.parse(1, usecols=[0], skiprows=[0], names=['Country'])
 print(df2.head())
 
 ############################################################################################
+
+"""
+Importing SAS files
+
+In this exercise, you'll figure out how to import a SAS file as a DataFrame using SAS7BDAT and pandas. 
+Both pandas and matplotlib.pyplot have already been imported as follows:
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+The data are adapted from the website of the undergraduate text book Principles of Econometrics by Hill, Griffiths and Lim.
+
+Instructions
+- Import the module SAS7BDAT from the library sas7bdat.
+- In the context of the file 'sales.sas7bdat', load its contents to a DataFrame df_sas, using the method .to_data_frame() on the object file.
+- Print the head of the DataFrame df_sas.
+- Execute your entire script to produce a histogram plot!
+
+"""
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Import sas7bdat package
+from sas7bdat import SAS7BDAT
+
+# Save file to a DataFrame: df_sas
+with SAS7BDAT('files/sales.sas7bdat') as file:
+    df_sas = file.to_data_frame()
+
+# Print head of DataFrame
+print(df_sas.head())
+
+# Plot histogram of DataFrame features (pandas and pyplot already imported)
+pd.DataFrame.hist(df_sas[['P']])
+plt.ylabel('count')
+plt.show()
+
+############################################################################################
+
+"""
+Importing Stata files
+
+Here, you'll gain expertise in importing Stata files as DataFrames using the pd.read_stata() function from pandas. 
+
+Instructions
+- Use pd.read_stata() to load the file 'disarea.dta' into the DataFrame df.
+- Print the head of the DataFrame df.
+- Visualize your results by plotting a histogram of the column disa10. We’ve already provided this code for you, so just run it!
+
+"""
+
+# Import pandas
+import pandas as pd
+
+# Load Stata file into a pandas DataFrame: df
+df = pd.read_stata('files/disarea.dta')
+
+# Print the head of the DataFrame df
+print(df.head())
+
+# Plot histogram of one column of the DataFrame
+pd.DataFrame.hist(df[['disa10']])
+plt.xlabel('Extent of disease')
+plt.ylabel('Number of countries')
+plt.show()
+
+############################################################################################
+
